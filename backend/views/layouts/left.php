ba4ad65f6a9cb7332b54fc9206cd4623dc96a52e
malweis <fidomol07@gmail.com>
2023-01-31 13:37:16 -0300

    Cambios de color

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -4,16 +4,14 @@ use common\helpers\PermisosHelpers;
 
 
 ?>
-<aside class="main-sidebar">
+<aside class="main-sidebar side-barA">
     <section class="sidebar">
         <?php
         $menuItems[] = ['label' => 'Inicio',
             'url' => 'index.php',
             'icon' => 'fa fa-gear',
+            'options' =>['class'=>'tarjetas'],
         ];
-        $items = [];
-        array_push($items, ['label' => 'Alumnos', 'url' => ['alumno/index'], 'icon' => 'fa fa-building', 'active' => \Yii::$app->controller->id == 'alumno']);
-        array_push($items, ['label' => 'Asignaturas', 'url' => ['asignatura/index'], 'icon' => 'fa fa-building', 'active' => \Yii::$app->controller->id == 'asignatura']);
         if (class_exists('\backend\controllers\MainModule')) {
             $itemsGeneral = \backend\controllers\MainModule::getMenuItems();
             if (!empty($itemsGeneral)) {
@@ -21,14 +19,15 @@ use common\helpers\PermisosHelpers;
                     'icon' => 'fa fa-gear',
                     'label' => 'Administracion',
                     'items' => $itemsGeneral,
-                    'url' => '#'
+                    'url' => '#',
+                    'options' =>['class'=>'borde'],
                 ];
             }
         }
         try {
             echo dmstr\widgets\Menu::widget(
                 [
-                    'options' => ['class' => 'sidebar-menu'],
+                    'options' => ['class' => 'sidebar-menu '],
                     'items' => $menuItems,
                 ]
             );
@@ -38,3 +37,35 @@ use common\helpers\PermisosHelpers;
     
     </section>
 </aside>
+
+<?php
+
+$CSS = <<<CSS
+/*This is modifying the btn-primary colors but you could create your own .btn-something class as well*/
+.side-barA{
+    background-color: #0f172a   ;
+    color: white;
+}
+.tarjetas  a{
+    color: #64748b;
+}
+.tarjetas:hover{
+    background-color: #94a3b8;
+    transition: 0.5s;
+    border-radius: 5px;
+    color: #fff;
+    
+}
+.tarjetas:hover a{
+    color: #fff;
+}
+.borde:hover{
+    border: 1px solid #94a3b8;
+    }
+    .borde a{
+        color: #64748b;
+
+    }
+CSS;
+
+$this->registerCss($CSS);
