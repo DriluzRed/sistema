018a93a2089a60d52fcb6c09f6eb59e630e79609
alarconat <alarconnati.ad06@gmail.com>
2023-02-27 21:28:35 -0300

    diseño muestra 1 del login

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -12,10 +12,12 @@ use yii\widgets\ActiveForm;
 
 <div class="login-box">
     <div class="login-logo">
-        
+       
     </div>
     <div class="login-box-body">
-        <p class="login-box-msg"><b>Control de Acceso</b></p>
+    <img src="../web/img/Logo-Fotriem-Blanco.png" alt="">
+        <h2 class="login-box-msg"><b>Control de Acceso</b></h2>
+        
         <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
         <div class="form-group has-feedback">
             <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'class' => 'form-control', 'placeholder' => 'Usuario']); ?>
@@ -43,3 +45,44 @@ use yii\widgets\ActiveForm;
     document.getElementById("login-form").onsubmit = function (e) {
     };
 </script>
+<!--Script de los iconos Agregado por Nati-->
+<script src="https://kit.fontawesome.com/a24879a822.js" crossorigin="anonymous"></script>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  color: white;
+}
+body{
+  display:flex;
+  justify-content:center;
+  align-items: center;
+  min-height: 100vh;
+  background: #23242a;
+}
+.login-box{
+  position: relative;
+  width: 380px;
+  height: 520px;
+  background: #1c1c1c;
+  border-radius: 8px;
+  overflow: hidden;
+  margin-left: 32.5%;
+}
+.login-box-body{
+  position:absolute;
+  inset: 2px;
+  background: #28292d;
+  border-radius: 8px;
+  z-index: 10;
+  padding: 50px 40px;
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
