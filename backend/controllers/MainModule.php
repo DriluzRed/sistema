53c9425ea64768d02b3d6d3e4bba375c8967753e
Arturo Gomez <arturorafa74@gmail.com>
2023-03-07 19:20:18 -0300

    icons

--- backend/controllers/MainModule.php
+++ backend/controllers/MainModule.php
@@ -29,13 +29,13 @@ class MainModule extends \yii\base\Module implements ModuleInterface
     {
         $items = array();
         $subitems_entidad = [];
-
-        (\Yii::$app->user->can('crearAlumno')) ?  array_push($items, ['label' => 'Total de Inscriptos', 'url' => ['alumno/index'], 'icon' => 'fa fa-building','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']):  '';
-        array_push($items, ['label' => 'Asignaturas', 'url' => ['asignatura/index'], 'icon' => 'fa fa-building','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'asignatura']);
-        array_push($items, ['label' => 'MAE - ESP - DIP', 'url' => ['programa/index'], 'icon' => 'fa fa-building','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'programa']);
-        array_push($items, ['label' => 'Carrera de Grado', 'url' => ['alumno/carreras'], 'icon' => 'fa fa-building','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']);
-        array_push($items, ['label' => 'Talleres', 'url' => ['alumno/talleres'], 'icon' => 'fa fa-building','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']);
-        array_push($items, ['label' => 'Libro de Graduados', 'url' => ['alumno/graduados'], 'icon' => 'fa fa-building','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']); 
+        array_push($items, ['label' => 'Inicio', 'url' => ['site/index'], 'icon' => 'fa fa-bars','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'site']);
+        (\Yii::$app->user->can('crearAlumno')) ?  array_push($items, ['label' => 'Total de Inscriptos', 'url' => ['alumno/index'], 'icon' => 'fa fa-solid fa-user','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']):  '';
+        array_push($items, ['label' => 'Asignaturas', 'url' => ['asignatura/index'], 'icon' => 'fa fa-solid fa-clipboard','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'asignatura']);
+        array_push($items, ['label' => 'MAE - ESP - DIP', 'url' => ['programa/index'], 'icon' => 'fa fa-solid fa-book','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'programa']);
+        array_push($items, ['label' => 'Carrera de Grado', 'url' => ['alumno/carreras'], 'icon' => 'fa fa-solid fa-book','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']);
+        array_push($items, ['label' => 'Talleres', 'url' => ['alumno/talleres'], 'icon' => 'fa fa-gear','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']);
+        array_push($items, ['label' => 'Libro de Graduados', 'url' => ['alumno/graduados'], 'icon' => 'fa fa-book','options' =>['class'=>'tarjetas'],  'active' => \Yii::$app->controller->id == 'alumno']); 
         return $items;
     }
 
