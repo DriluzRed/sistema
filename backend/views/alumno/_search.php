1bbba0769c4515ceae0aa86877f1d18b51ff526d
malweis <fidomol07@gmail.com>
2023-03-08 12:25:21 -0300

    hay filtros

--- backend/views/alumno/_search.php
+++ backend/views/alumno/_search.php
@@ -25,45 +25,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'country_id') ?>
 
-    <?php // echo $form->field($model, 'low_line_number') ?>
-
-    <?php // echo $form->field($model, 'phone') ?>
-
-    <?php // echo $form->field($model, 'email') ?>
-
-    <?php // echo $form->field($model, 'address') ?>
-
-    <?php // echo $form->field($model, 'age') ?>
-
-    <?php // echo $form->field($model, 'programa_id') ?>
-
-    <?php // echo $form->field($model, 'campus') ?>
-
-    <?php // echo $form->field($model, 'subsidiary') ?>
-
-    <?php // echo $form->field($model, 'enrrolment_date') ?>
-
-    <?php // echo $form->field($model, 'contract_number') ?>
-
-    <?php // echo $form->field($model, 'year') ?>
-
-    <?php // echo $form->field($model, 'promotion_year') ?>
-
-    <?php // echo $form->field($model, 'born_at') ?>
-
-    <?php // echo $form->field($model, 'promotion') ?>
-
-    <?php // echo $form->field($model, 'document_front_file') ?>
-
-    <?php // echo $form->field($model, 'document_back_file') ?>
-
-    <?php // echo $form->field($model, 'status') ?>
-
-    <?php // echo $form->field($model, 'study_certificate_file') ?>
-
-    <?php // echo $form->field($model, 'finded_ips') ?>
-
-    <?php // echo $form->field($model, 'finded_ruc') ?>
+   
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
