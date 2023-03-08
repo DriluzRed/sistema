79a9d0cff92b2527bd64d04d02af58ad2a45c8f7
malweis <fidomol07@gmail.com>
2023-03-08 12:25:21 -0300

    hay filtros

--- backend/views/programa/_search.php
+++ backend/views/programa/_search.php
@@ -24,6 +24,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'desc') ?>
 
+    
+
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
